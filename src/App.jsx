91ee98032fba941c1f9eeeebd9a0297078d6bdc4
MuzhdaN <muzhda.noorzad4@gmail.com>
2024-06-01 17:45:18 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+
+export default function App() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Hero />
+      <Projects />
+
+    </>
+  )
+}
