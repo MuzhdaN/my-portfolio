4c3818680cd980c8e5063ab4e5a5a5ab294f961f
MuzhdaN <muzhda.noorzad4@gmail.com>
2024-06-01 23:23:15 -0500

    Add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import Hero from "./components/Hero";
-// import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
+
   return (
     <>
-      {/* <Navbar /> */}
-      <Hero />
-      <Projects />
-
+      <div className={`${darkMode ? "dark bg-neutral-900" : ""}`}>
+        <Hero darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        <Projects darkMode={darkMode} />
+        <Skills darkMode={darkMode}/>
+        <Footer />
+      </div>
     </>
   )
 }
