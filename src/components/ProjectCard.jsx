0b97593f79f880ebf390bb4d8f2fd28b76570aed
MuzhdaN <muzhda.noorzad4@gmail.com>
2024-06-02 20:01:57 -0500

    Add project section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,37 @@
 import { FaGithub } from 'react-icons/fa6';
 import { CiLocationArrow1 } from "react-icons/ci";
 import logo from '../assets/react.svg';
+import { useState } from 'react';
 
 const ProjectCard = ({ project, darkMode }) => {
+
+  const [showFullDescription, setShowFullDesciption] = useState(false);
+
+  let description = project.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0,90) + "...";
+  }
+
   return (
     <div className={`w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 px-4 mb-4 ${darkMode ? 'dark' : ''}`}>
       <div className={`container-xl lg:container bg-white rounded-lg shadow-lg relative m-5 p-6 ${darkMode ? 'dark:bg-gray-800 text-white' : ''}`}>
-        <img src={logo} alt="hi" />
+        <img src={project.imageSrc} alt="project title" className='mb-5' />
 
-        <h2 className={`text-xl font-bold text-indigo-500 mb-6 text-center ${darkMode ? 'text-white' : ''}`}>
+        <h2 className={`text-xl font-bold text-indigo-500 my-2 text-center ${darkMode ? 'text-white' : ''}`}>
           {project.title}
         </h2>
 
-        <p className={`${darkMode ? 'text-gray-300' : ''} mb-6`}> {project.description} </p>
+        <p className={`${darkMode ? 'text-gray-300' : ''} mb-1`}>
+          {description} 
+        </p>
+
+        <button 
+              onClick={() => setShowFullDesciption((prevState) => !prevState)} 
+              className="text-indigo-700 mb-3 hover:text-indigo-900"
+            >
+              {showFullDescription ? 'less' : 'More'}
+            </button>
 
         <ul className='flex flex-row flex-wrap justify-center gap-4 mb-5'>
           {project.skills.map((skill, index) => (
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,11 @@
-import projectsData from './projects.json';
+import projectsData from '../json/projects.json';
 import ProjectCard from './ProjectCard';
 
 const Projects = ({ darkMode }) => {
   return (
-    <section className="mx-4 my-2">
+    <section className="mx-4 my-2 p-3">
       <div className={`p-5 rounded-xl ${darkMode ? 'dark:bg-transparent' : 'bg-indigo-50'}`}>
-        <h2 className={`dark:text-white text-3xl font-extrabold text-indigo-700  mb-6 text-center`}>
+        <h2 className={`dark:text-white text-3xl font-extrabold text-indigo-700  my-6 text-center`}>
           Projects
         </h2>
 
